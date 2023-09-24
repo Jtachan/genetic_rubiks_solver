9d57af6b8509a5c2a27373768d7686dffb77f3e4
Jtachan <76098522+Jtachan@users.noreply.github.com>
2023-09-24 20:07:38 +0200

    Test: Expanded initialization test

--- unittests/test_cube.py
+++ unittests/test_cube.py
@@ -1,21 +1,29 @@
 """Module to test the correct behaviour of the cube"""
 import pytest
-from rubik import RubiksCube, CubeFace
-import numpy as np
+
+from rubik import CubeFace, RubiksCube
 
 
 def test_initialization():
     """Correct initialization of the cube"""
     cube = RubiksCube()
     assert cube.is_solved(), "The cube has not been initialized as a solved cube."
-    print(cube)
+
+    cube.scramble()
+    # And odd number of random moves cannot get as result a solved cube
+    assert not cube.is_solved(), "Could not scramble the cube correctly"
 
     cube = RubiksCube.from_color_code(
-        "GGGGGGGGGoooooooooYYYYYYYYYrrrrrrrrrWWWWWWWWWbbbbbbbbb")
+        "GGGGGGGGGoooooooooYYYYYYYYYrrrrrrrrrWWWWWWWWWbbbbbbbbb"
+    )
     assert cube.is_solved(), "Issue initializing the cube from a string."
-    print(cube)
+
+    cube = RubiksCube.from_color_code(
+        "BGBGGGWRGBOYYRBGYORGYBWGRGWYGOGOBOGOYGYGYGRGWYOROBGRBG"
+    )
+    assert not cube.is_solved(), "Issue initializing a scrambled cube from a string."
 
 
-@pytest.mark.parametrize("face", [face for face in CubeFace])
+@pytest.mark.parametrize("face", list(CubeFace))
 def test_faces_moves(face: CubeFace):
     """Correct rotation of all the main cube faces"""
